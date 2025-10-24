1544df4bedf3c86065ac5a72ce9c8d790e88c6a7
Samitsingh001 <samit.singh@ust.com>
2025-10-24 10:22:07 +0530

    MinikubeSampleApplicationTests.java

--- src/test/java/com/minikube/sample/MinikubeSampleApplicationTests.java
+++ src/test/java/com/minikube/sample/MinikubeSampleApplicationTests.java
@@ -3,15 +3,23 @@ package com.minikube.sample;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MinikubeSampleApplicationTests {
 
-    @Test
-    public void contextLoads() {
-        // This test ensures that the Spring application context loads successfully.
+    private final ApplicationContext context;
+
+    public MinikubeSampleApplicationTests(ApplicationContext context) {
+        this.context = context;
     }
 
+    @Test
+    public void contextLoads() {
+        assertThat(context).isNotNull(); // Ensures context loaded properly
+    }
 }
