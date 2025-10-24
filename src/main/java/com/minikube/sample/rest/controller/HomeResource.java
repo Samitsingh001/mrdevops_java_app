2865a878f6e5fb85187bec32b11f3c47a20436ee
Samitsingh001 <samit.singh@ust.com>
2025-10-24 14:51:21 +0530

    HomeResource.java

--- src/main/java/com/minikube/sample/rest/controller/HomeResource.java
+++ src/main/java/com/minikube/sample/rest/controller/HomeResource.java
@@ -1,28 +1,33 @@
 package com.minikube.sample.rest.controller;
-
+ 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.minikube.sample.properties.PropertiesConfig;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Lookup;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+ 
 /**
- * @author Gorantla, Eresh
- * @created 06-12-2018
- */
+* @author Gorantla, Eresh
+* @created 06-12-2018
+*/
 @RestController
 @RequestMapping("/home")
 public class HomeResource {
-
-    private final PropertiesConfig config;
-
-    @Autowired
-    public HomeResource(PropertiesConfig config) {
-        this.config = config;
-    }
-
+ 
+	private final PropertiesConfig config;
+ 
+	@Autowired
+	public HomeResource(PropertiesConfig config) {
+		this.config = config;
+	}
+ 
     @GetMapping("/data")
     public ResponseEntity<ResponseData> getData() {
         ResponseData responseData = new ResponseData();
@@ -32,43 +37,13 @@ public class HomeResource {
         responseData.setValue(config.getTest());
         return new ResponseEntity<>(responseData, HttpStatus.OK);
     }
-
-    public static class ResponseData {
+ 
+    @Getter
+    @Setter
+    public class ResponseData {
         private String name;
         private Integer id;
         private String place;
         private String value;
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public Integer getId() {
-            return id;
-        }
-
-        public void setId(Integer id) {
-            this.id = id;
-        }
-
-        public String getPlace() {
-            return place;
-        }
-
-        public void setPlace(String place) {
-            this.place = place;
-        }
-
-        public String getValue() {
-            return value;
-        }
-
-        public void setValue(String value) {
-            this.value = value;
-        }
     }
 }
