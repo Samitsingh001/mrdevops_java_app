eda99ba1023d6ef33021354dde217b807b50d931
Samitsingh001 <samit.singh@ust.com>
2025-10-24 10:40:31 +0530

    MinikubeSampleApplicationTests.java

--- src/test/java/com/minikube/sample/MinikubeSampleApplicationTests.java
+++ src/test/java/com/minikube/sample/MinikubeSampleApplicationTests.java
@@ -1,24 +1,14 @@
 package com.minikube.sample;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
-import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-@RunWith(SpringRunner.class)
 @SpringBootTest
-public class MinikubeSampleApplicationTests {
-
-    @SuppressWarnings("java:S6813") // Suppress Sonar warning about field injection
-    @Autowired
-    private ApplicationContext context; // Field injection is acceptable in test classes
+class MinikubeSampleApplicationTests {
 
     @Test
-    public void contextLoads() {
-        assertThat(context).isNotNull(); // Ensures context loaded properly
+    void contextLoads() {
+        // Test to ensure the Spring application context loads successfully
     }
+
 }
